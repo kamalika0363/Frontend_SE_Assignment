418668e39bdb484ddaef4d22ffc47d39147753e9
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-08-18 22:25:03 +0530

    Created a basic react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <h1 className='min-h-screen bg-slate-500 grid place-items-center'>
+          FrontEnd SE Assignment
+      </h1>
+    </>
+  )
+}
+
+export default App
