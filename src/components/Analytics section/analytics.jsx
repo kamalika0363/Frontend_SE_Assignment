00b87bf967bd9fd8f2bea047c08b1fd9e24a34a0
Shivam Mishra <shivam.r.mishra9120@gmail.com>
2023-08-19 20:14:27 +0530

    [CHANGED] Added padding to anayltics section as requested

--- src/components/Analytics section/analytics.jsx
+++ src/components/Analytics section/analytics.jsx
@@ -3,7 +3,7 @@ import Statistics from "./statistics";
 
 function Analytics(){
     return(
-        <div className=" m-[2rem] lg:m-[4rem] p-24">
+        <div className=" m-[2rem] lg:m-[4rem] sm:p-24 py-12">
          <Description/>
          <Statistics/>
         </div>
