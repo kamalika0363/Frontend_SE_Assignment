4bb6589dd64c3cf317d806d4d797f3cd4a813c56
0amirk <amir.kamal09@gmail.com>
2023-08-19 19:36:16 +0530

    The requested changes weere made

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -54,12 +54,12 @@ function Features (){
                     Features
                 </p>
 
-                <p className=" text-4xl text-[#101828] mb-5">
+                <p className=" text-4xl text-[#101828] mb-5 font-semibold">
                 Analytics that feels like it’s from the future
 
                 </p>
 
-                <p className="text-xl text-[#667085]">
+                <p className="text-xl text-[#667085] sm:text-2xl sm:px-12 lg:px-72">
                 Powerful, self-serve product and growth analytics to help you convert, engage, and retain more users. Trusted by over 4,000 startups.
                 </p>
 
