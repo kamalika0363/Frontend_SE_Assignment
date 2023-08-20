2acbb6a5e6b68f9fdf8052835b2c219fc3e75ce2
pranaytiwariii <pranaytiwariprpk@gmail.com>
2023-08-20 21:05:46 +0530

    [Added]feature section

---
+++ components/Feature/Features.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import './Feature.css'
+import mail from '../../public/Features/cards/mail.svg'
+import zap from '../../public/Features/cards/zap.svg'
+import barchart from '../../public/Features/cards/bar-chart-2.svg'
+import arrow from '../../public/Features/cards/arrow-right.svg'
+
+
+export default function Features() {
+  return (
+    
+ <section className="flex mx-2 md:mx-4 flex-col items-center gap-12 pt-24">
+  {/* ----------------------header section---------------------- */}
+   <div className="top-container">
+      <div className="badge-section text-xs m-auto text-[#6941C6] flex gap-3 pt-1 pr-1 pb-1 pl-1 rounded-2xl w-max items-center justify-center bg-gray-100">
+      <p className="label-1 bg-[#F9F5FF] pl-1 pr-1 justify-start rounded-2xl">
+      Features
+      </p>
+      </div>
+     <div className="heading-section mt-3 flex flex-col gap-4 items-center">
+     <h1 className="text-2xl md:text-4xl font-semibold text-center flex-wrap">
+        Cutting-edge features for advanced analytics
+     </h1>
+     <p className="text-xl text-center text-[#667085] sm:text-xl sm:px-12 lg:px-72">
+        Powerful, self-serve product and growth analytics to help you convert, engage, and retain more users. Trusted by over 4,000 startups.
+     </p>
+      </div>
+   </div>
+
+   {/* ----------------------image section---------------------- */}
+    <div className="img-container relative">
+       <div className="img-section relative">
+          <img className="  lg:w-[80%] m-auto" src="public/Features/mock_dashboard.png" />
+          <img className="mobile absolute top-48 z-2 rounded-3xl w-[244px] h-[496px] lg:w-[22%] lg:h-[80%] md:w-[22%] md:h-[65%] sm:w-[22%] sm:h-[65%] m-auto " src="public/Features/mobile.png" />
+       </div>
+  {/* ----------------------footer cards section---------------------- */}
+       <div className="cards flex flex-wrap gap-6 pt-20 py-10 ">
+         <div className="card1n2 flex lg:mx-auto flex-wrap md:flex-wrap mx-auto justify-center">
+            <div className="cards1  w-96  text-center sm:py-10">
+         <div className="mail mx-44">
+             <img src={mail} alt="" />
+         </div>
+         <p className=" text-xl">
+           Share team inboxes
+         </p>
+         <p className=" text-base text-[#667085] text-center">
+         Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop.
+         </p>
+         <button className="bottom flex items-center mx-auto text-[#6941C6]"> <span>Learn more </span><img src={arrow} alt="" />
+         </button>
+       </div>
+
+       <div className="cards2 w-96 text-center mt-10">
+         <div className="zap mx-44">
+             <img src={zap} alt="" />
+         </div>
+         <p className=" text-xl">
+         Deliver instant answers
+         </p>
+         <p className=" text-base text-[#667085] text-center">
+         An all-in-one customer service platform that helps you balance everything your customers need to be happy.
+         </p>
+         <button className="bottom flex mx-auto text-[#6941C6]">Learn more <img src={arrow} className='' alt="" />
+         </button>
+            </div>
+           </div>
+            <div className="cards3 flex-col w-96 text-center mt-9 lg:flex mx-auto m-5">
+         <div className="barchart mx-44">
+             <img src={barchart} alt="" />
+         </div>
+         <p className=" text-xl">
+         Manage your team with reports
+         </p>
+         <p className=" text-base text-[#667085] text-center">
+          Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks.
+          Learn more
+         </p>
+         <button className="bottom flex mx-auto text-[#6941C6]">Learn more <img src={arrow} alt="" />
+         </button>
+       </div>
+       </div>
+   
+
+    </div>
+ </section>
+
+  )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Index from "../components/FAQSection";
+// import Index from "../components/FAQSection";
+import Features from "../components/Feature/Features";
 function App() {
   return (
     <main>
-      <h1 className='min-h-screen grid place-items-center'>
-        FrontEnd SE Assignment
-        <Index />
-      </h1>
+    {/* <Index /> */}
+    <Features />
     </main>
   );
 }
